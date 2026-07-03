c29efa31339cad1bfcfcbe96edff05ca217e87ac
Tridip Baksi <tridipbaksi192@gmail.com>
2026-07-03 12:04:39 +0530

    Add response DTOs for antecedent reports and police records

---
+++ src/main/java/com/secure/policerecord/response/AntecedentReportReponse.java
@@ -0,0 +1,24 @@
+package com.secure.policerecord.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AntecedentReportResponse {
+    private String id;
+    private String reportNumber;
+    private String citizenReference;
+    private String officerName;
+    private String convictionStatus;
+    private Integer pendingCases;
+    private Boolean blacklistFlag;
+    private String overallStatus;
+    private String reportHash;
+    private String blockchainTxId;
+    private String submittedAt;
+}
---
+++ src/main/java/com/secure/policerecord/response/PoliceRecordResponse.java
@@ -0,0 +1,24 @@
+package com.secure.policerecord.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PoliceRecordResponse {
+    private String id;
+    private String recordId;
+    private Integer version;
+    private String recordType;
+    private String content;
+    private String previousHash;
+    private String currentHash;
+    private String actionType;
+    private String officerName;
+    private String blockchainTxId;
+    private String createdAt;
+}
